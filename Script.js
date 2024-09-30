e4e20b3b21408e979718a7d0d5438c9bdea336f2
Guimartinssilva <guimartinssilva23@gmail.com>
2024-09-30 20:12:25 -0300

    Exercícios1 de operadores

---
+++ Script.js
@@ -0,0 +1,29 @@
+let valor1 = 3
+let valor2 = 4
+
+resultado = valor1 + valor2
+
+console.log(resultado)
+
+let valor3 = 3
+let valor4 = 5
+let valor5 = 2
+
+resultado1 = (valor3 * valor4) / valor5
+
+console.log(resultado1)
+
+let valor6 = 5
+let valor7 = 4
+let valor8 = -1
+
+resultado2 = (valor6 - valor7) * valor8
+
+console.log(resultado2)
+
+let valor9 = 234
+let valor10 = 5
+
+resultado3 = valor9 % valor10
+
+console.log(resultado3)
