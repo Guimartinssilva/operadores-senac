d15cffceeb363acc7255308101e0d7959177e56c
Guimartinssilva <guimartinssilva23@gmail.com>
2024-09-30 21:51:07 -0300

    Exercícios4 de operadores

---
+++ Script3.js
@@ -0,0 +1,8 @@
+const a = true
+const b = false
+const c = true
+
+console.log(a || b, "F/V") //TRUE
+console.log(b || c, "F/V") //TRUE
+console.log(a || c, "F/V") //TRUE
+console.log(a || b || c, "F/V") //TRUE
