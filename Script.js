ddde9bf838877ab3fa3c2c5557b59c59a4ed1128
Guimartinssilva <guimartinssilva23@gmail.com>
2024-09-30 21:35:17 -0300

    Exercícios2 de operadores

--- Script.js
+++ Script.js
@@ -1,29 +1,37 @@
-let valor1 = 3
-let valor2 = 4
+// let valor1 = 3
+// let valor2 = 4
 
-resultado = valor1 + valor2
+// resultado = valor1 + valor2
 
-console.log(resultado)
+// console.log(resultado)
 
-let valor3 = 3
-let valor4 = 5
-let valor5 = 2
+// let valor3 = 3
+// let valor4 = 5
+// let valor5 = 2
 
-resultado1 = (valor3 * valor4) / valor5
+// resultado1 = (valor3 * valor4) / valor5
 
-console.log(resultado1)
+// console.log(resultado1)
 
-let valor6 = 5
-let valor7 = 4
-let valor8 = -1
+// let valor6 = 5
+// let valor7 = 4
+// let valor8 = -1
 
-resultado2 = (valor6 - valor7) * valor8
+// resultado2 = (valor6 - valor7) * valor8
 
-console.log(resultado2)
+// console.log(resultado2)
 
-let valor9 = 234
-let valor10 = 5
+// let valor9 = 234
+// let valor10 = 5
 
-resultado3 = valor9 % valor10
+// resultado3 = valor9 % valor10
 
-console.log(resultado3)
+// console.log(resultado3)
+
+let valor11 = 23
+let valor12 = 15
+
+console.log(valor11===valor12)
+console.log(valor11!==valor12)
+console.log(valor11>valor12)
+console.log(valor11<valor12)
---
+++ Script1.js
@@ -0,0 +1,7 @@
+let valor1 = 23
+let valor2 = 15
+
+console.log("O primeiro número é igual ao segundo?",  valor1===valor2)
+console.log("O primeiro número é diferente ao segundo?", valor1!==valor2)
+console.log("O primeiro número é maior que o segundo?", valor1>valor2)
+console.log("O primeiro número é menor que o segundo?", valor1<valor2)
